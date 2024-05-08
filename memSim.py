4cfaee4dbd2b607630d646b6539f48124fc6c5fa
amalekia <amalekia@calpoly.edu>
2024-05-07 18:01:54 -0700

    some changes

--- memSim.py
+++ memSim.py
@@ -1,25 +1,98 @@
+import sys
 
 class TLB:
     def __init__(self):
-        self.entries = []
+        #list of entries that hold most recently accesses page numbers with their corresponding frame numbers
+        self.entries = {}   #dictionary that holds the page number and the frame number
         self.size = 16
 
 class PageTable:
     def __init__(self):
-        self.entries = []
-        self.size = 8
-        self.pageSize = 256    # 256 bytes
+        #dictionary that holds the page number and the frame number
+        self.pages = {}     #dictionary that holds the page number and the frame number
+        self.numPages = 8      
+        self.pageSize = 256    # 256 bytes per page
 
 class PhysicalMemory:
-    def __init__(self):
-        self.pages = []
-        self.size = 32
+    def __init__(self, frames):
+        #memory which frames map to
+        self.frames = {}    #dictionary that holds the frame number and the data
+        self.numframes = frames
 
-def memSim(numFrames):
-    
-    
+def FIFO():
+    #implement FIFO page replacement algorithm
+    pass
 
+def LRU():
+    #implement LRU page replacement algorithm
+    pass
+
+def OPT():
+    #implement LFU page replacement algorithm
+    pass
+
+#default page replacement alg
+str pageRepAlg = "FIFO"
+
+#instance of the classes
+TLB tlb
+PageTable pageTable
+PhysicalMemory physMem
+
+#page faults counter and tlb hit/miss counter
+int pageFaults = 0
+int tlbHits = 0
+int tlbMisses = 0
+
+#function to simulate memory
+def memSim():
+    #read in the file
+    with open("addresses.txt", "r") as f:
+        for line in f:
+            #parse the address
+            addr = int(line, 16)
+            page = (addr & 0xFF00) >> 8
+            offset = addr & 0xFF
+            frame = -1
+
+            #check the TLB
+            if tlb.entries.get(page) != None:
+                tlbHits += 1
+                frame = tlb.entries.get(page)
+            else:
+                tlbMisses += 1
+                #check the page table
+                if pageTable.pages.get(page) != None:
+                    frame = pageTable.pages.get(page)
+                else:
+                    #page fault
+                    pageFaults += 1
+                    #implement page replacement algorithm
+                    if pageRepAlg == "LRU":
+                        frame = LRU()
+                    elif pageRepAlg == "OPT":
+                        frame = OPT()
+                    else:
+                        frame = FIFO()
+
+                    #update the page table
+                    pageTable.pages[page] = frame
+
+                #update the TLB
+                tlb.entries[page] = frame
+
+            #get the data from the physical memory
+            data = physMem.frames.get(frame * 256 + offset)
+            print(data)
 
 if __name__ == '__main__':
-    numFrames = input("Enter the number of frames: ")
-    memSim(numFrames)
+    for i in range(len(sys.argv)):
+        if i == 0:
+            pageTable = PhysicalMemory(int(arg))
+        if i == 1:
+            if arg == "FIFO" or arg == "LRU" or arg == "OPT":
+                pageRepAlg = arg
+            else:
+                print("Invalid page replacement algorithm")
+                sys.exit(1)
+    memSim()
